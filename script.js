d2af783ade30e195d5ceab0043faaee8c7d37868
Zane Sterling <sterling.zane@gmail.com>
2015-03-28 16:25:12 -0400

    Add image loading stuffs

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var images = {
+};
+
+function loadImages() {
+	var imgs = $('img');
+	for (var i = 0; i < imgs.length; i++) {
+		images[imgs[i].id] = imgs[i];
+	}
+	console.log(images);
+}
+
+loadImages();
 
 function drawImage(img, transx, transy, sclx, scly, rot) {
     ctx.translate(transx, transy);
@@ -20,4 +32,11 @@ function velo() {
 
 im.onload = function() {
     setInterval(velo, 10);
+}
+
+function draw() {
+	drawCharacter(0, 0);
+}
+
+function drawCharacter(x, y) {
 }
