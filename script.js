9f8d3a82d600e967b6b99a23e3d8b020b3263c70
Josh Hofing <joshhofing@gmail.com>
2015-03-28 16:15:24 -0400

    velovelo

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.fillRect(0, 0, 10, 10);
+
+function drawImage(img, transx, transy, sclx, scly, rot) {
+    ctx.translate(transx, transy);
+    ctx.rotate(rot);
+    ctx.drawImage(img, 0, 0);
+    ctx.rotate(-rot);
+    ctx.translate(-transx, -transy);
+}
+
+var im = document.getElementById('velo');
+im.onload = function() {
+    console.log("hey");
+    drawImage(im, 0, 0, 1, 1, 0);
+}
