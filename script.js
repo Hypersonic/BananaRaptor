11052d46f26834f0855ca13657003264df5dc36f
Josh Hofing <joshhofing@gmail.com>
2015-03-28 16:19:56 -0400

    spin velovelo

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function drawImage(img, transx, transy, sclx, scly, rot) {
 }
 
 var im = document.getElementById('velo');
+var rot = 0;
+
+function velo() {
+    drawImage(im, 0, 0, 1, 1, rot);
+    rot += 2;
+}
+
 im.onload = function() {
-    console.log("hey");
-    drawImage(im, 0, 0, 1, 1, 0);
+    setInterval(velo, 10);
 }
